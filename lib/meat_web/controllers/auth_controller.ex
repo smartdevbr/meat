89fc36ba49eb9297a0198f7a63b23153593a6906
Gustavo Oliveira <oliveira.gustavo@totvs.com.br>
2019-02-21 07:05:51 -0300

    extracting information of ueberauth

--- lib/meat_web/controllers/auth_controller.ex
+++ lib/meat_web/controllers/auth_controller.ex
@@ -2,10 +2,14 @@ defmodule MeatWeb.AuthController do
     use MeatWeb, :controller
     plug Ueberauth
 
-  def callback(conn, params) do
-    IO.inspect(conn)
-    IO.puts("-------------------------------------- callback")
-    IO.inspect(params)
+  def callback(%{assigns: %{ueberauth: auth}} = conn, %{"provider" => provider}) do
+    %{
+    name: auth.info.name,
+    token: auth.credentials.token,
+    email: auth.info.email,
+    provider: provider
+    }
+
   end
 
   def logout(conn, params) do
